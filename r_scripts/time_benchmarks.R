aea1855f524e62c77cbcd71b98de4f831fce8bb4
nils <nils.palme@gmail.com>
2025-01-29 19:31:45 +0100

    rework plots

--- r_scripts/time_benchmarks.R
+++ r_scripts/time_benchmarks.R
@@ -8,15 +8,19 @@ bloom_insert <- "tables/benchmark_bloom_insert.csv"
 bloom_query_negative <- "tables/benchmark_bloom_query_negative.csv"
 bloom_query_positive <- "tables/benchmark_bloom_query_positive.csv"
 
-table <- read_csv(bloom_query_positive)
+table <- read_csv(adapted_insert)
 
 
 table <- table |>
   mutate(time_in_s = time / 10**9)
 
+
 graph <- table |>
   ggplot(aes(x = name, y=time_in_s)) +
+  scale_y_continuous(limits = c(0, 35)) +
   geom_col() +
   labs(x = "Filter", y = "Time (s)") 
-
+  
 graph
+
+
